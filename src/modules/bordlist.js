84732e138684c4658dce5e90ff3ecf342ca67d52
Walid Kabboussa <walidkabou@gmail.com>
2023-03-08 23:17:04 +0100

    fetch Api and add function to diplay data on the main page

---
+++ src/modules/bordlist.js
@@ -0,0 +1,6 @@
+export default class BoardList {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}
---
+++ src/modules/getdata.js
@@ -0,0 +1,28 @@
+export const getData = async () => {
+  const request = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yILAt9hHFpCy3D3LuPBP/scores/');
+  const data = await request.json();
+  data.results.sort((a, b) => b.score - a.score);
+
+  const displayData = async (data) => {
+    data.results.forEach((elemenT) => {
+      document.querySelector('.scores').innerHTML += `
+         <p> ${elemenT.user} : ${elemenT.score}</p>
+        `;
+    });
+  };
+  displayData(data);
+};
+
+export const postData = async (item) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yILAt9hHFpCy3D3LuPBP/scores/', {
+
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user: item.name,
+      score: item.score,
+    }),
+  });
+  const responseload = await response.json();
+  return responseload;
+};
